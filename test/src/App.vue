0cd96de2e0d3c0276e1b9df13dfa3138a31d635f
chilling-Yuji <yuji20021126@gmail.com>
2020-11-11 19:58:57 +0900

    first-edit

--- test/src/App.vue
+++ test/src/App.vue
@@ -7,11 +7,13 @@
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import CommonHeader from "./components/CommonHeader/"
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    CommonHeader
   }
 }
 </script>
